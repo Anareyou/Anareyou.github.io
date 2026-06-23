e2fa79b3ddf872e4621e2e1c3a8ef4f81b00adb5
Anareyou <lih2027@outlook.com>
2026-06-23 19:22:06 +0800

    Site updated: 2026-06-23 19:22:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/23/hello-world/","2025/04/06/markdown语法/","2026/06/23/hexo部署/","2026/06/23/git/"];function toRandomPost(){
+var posts=["2026/06/23/hello-world/","2026/06/23/git/","2025/04/06/markdown语法/","2026/06/23/hexo部署/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
